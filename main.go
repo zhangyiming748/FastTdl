d5e8831ae5b2c789fc705cf6d9756818a13c437c
zen <hasee>
2024-11-04 20:16:18 +0800

    修改权限只针对Linux容器

--- main.go
+++ main.go
@@ -4,24 +4,27 @@ import (
 	"fmt"
 	"github.com/zhangyiming748/FastTdl/tdl"
 	"github.com/zhangyiming748/FastTdl/util"
+	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 func main() {
 	defer func() {
-		home, err := os.UserHomeDir()
-		if err != nil {
-			fmt.Println("无法获取用户的个人文件夹目录:", err)
+		if runtime.GOOS == "linux" {
+			home, err := os.UserHomeDir()
+			if err != nil {
+				fmt.Println("无法获取用户的个人文件夹目录:", err)
+			}
+			dir := filepath.Join(home, "Downloads")
+			if path := os.Getenv("TDL"); path != "" {
+				dir = path
+			}
+			target := filepath.Join(dir, "telegram")
+			log.Printf("最终下载的文件夹: %s\n", target)
+			util.Chown(target, 1000, 1000)
 		}
-		dir := filepath.Join(home, "Downloads")
-		if path := os.Getenv("TDL"); path != "" {
-			dir = path
-		}
-		fmt.Printf("用户的个人文件夹目录: %s\n", home)
-		fmt.Printf("用户的下载文件夹目录: %s\n", dir)
-		target := filepath.Join(dir, "telegram")
-		util.Chown(target, 1000, 1000)
 	}()
 	urls := util.ReadByLine("post.link")
 	proxy := os.Getenv("PROXY")
