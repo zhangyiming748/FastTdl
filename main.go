83eba8aaf9d58fd11a6918b57a99c4fb195af7b5
zen <hasee>
2024-11-04 19:47:35 +0800

    尝试在程序结束之前恢复文件权限

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/zhangyiming748/FastTdl/tdl"
 	"github.com/zhangyiming748/FastTdl/util"
 	"os"
+	"path/filepath"
 )
 
 func main() {
+	defer func() {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println("无法获取用户的个人文件夹目录:", err)
+		}
+		dir := filepath.Join(home, "Downloads")
+		if path := os.Getenv("TDL"); path != "" {
+			dir = path
+		}
+		fmt.Printf("用户的个人文件夹目录: %s\n", home)
+		fmt.Printf("用户的下载文件夹目录: %s\n", dir)
+		target := filepath.Join(dir, "telegram")
+		util.Chown(target, 1000, 1000)
+	}()
 	urls := util.ReadByLine("post.link")
 	proxy := os.Getenv("PROXY")
 	if proxy == "" {
