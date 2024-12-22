f9338b29e3b6323ceda99a965a5cef96c2c51e94
zen <mbp>
2024-12-22 14:26:53 +0800

    使用ai简化脚本

--- rotate.sh
+++ rotate.sh
@@ -1,56 +1,40 @@
 #!/bin/bash
 
-
 # 指定要查找的扩展名
 extension="mp4"
-root="/mnt/c/Users/zen/Downloads/media/维拉莎莎"
+root="/Users/zen/Downloads/telegram/tifa"
 
-right=$root/toRight
-# 遍历当前目录下的所有文件
-for file in "$right"/*.$extension; do
-	# 检查文件是否具有指定的扩展名
-	if [[ $file == *.$extension ]]; then
-		# 提取文件名和扩展名
-		filename=$(basename "$file")
-		basename_without_ext="${filename%.*}"
-		ext="${filename##*.}"
-		aftername=${basename_without_ext}_rotate.mp4
-		# 打印分开的文件名和扩展名
-		echo "文件名： $basename_without_ext"
-		echo "扩展名： $ext"
-		ffmpeg -i $file -vf transpose=1 -c:v h264_nvenc -preset medium -c:a copy -map_chapters -1 $right/$aftername
-		if [ $? -eq 0 ]; then
-			echo "旧文件名: $filename 新文件名: $aftername"
-			echo "done"
-			rm $file
-		else
-			echo "ffmpeg command failed for file: $file"
-		fi
-		echo "done"
-	fi
-done
+process_files() {
+    local dir=$1
+    local transpose_value=$2
 
-left=$root/toLeft
-# 遍历当前目录下的所有文件
-for file in "$left"/*.$extension; do
-	# 检查文件是否具有指定的扩展名
-	if [[ $file == *.$extension ]]; then
-		# 提取文件名和扩展名
-		filename=$(basename "$file")
-		basename_without_ext="${filename%.*}"
-		ext="${filename##*.}"
-		aftername=${basename_without_ext}_rotate.mp4
-		# 打印分开的文件名和扩展名
-		echo "文件名： $basename_without_ext"
-		echo "扩展名： $ext"
-		ffmpeg -i $file -vf transpose=2 -c:v h264_nvenc -preset medium -c:a copy -map_chapters -1 $left/$aftername
-		if [ $? -eq 0 ]; then
-			echo "旧文件名: $filename 新文件名: $aftername"
-			echo "done"
-			rm $file
-		else
-			echo "ffmpeg command failed for file: $file"
-		fi
-		echo "done"
-	fi
-done
+    # 遍历指定目录下的所有文件
+    for file in "$dir"/*.$extension; do
+        # 检查文件是否具有指定的扩展名
+        if [[ -f $file ]]; then
+            # 提取文件名和扩展名
+            filename=$(basename "$file")
+            basename_without_ext="${filename%.*}"
+            aftername="${basename_without_ext}_rotate.mp4"
+
+            # 打印分开的文件名和扩展名
+            echo "文件名： $basename_without_ext"
+            echo "扩展名： $extension"
+
+            # 执行ffmpeg命令
+            ffmpeg -i "$file" -vf "transpose=$transpose_value" -c:v libx265 -preset medium -c:a copy -map_chapters -1 "$dir/$aftername"
+            if [ $? -eq 0 ]; then
+                echo "旧文件名: $filename 新文件名: $aftername"
+                echo "done"
+                rm "$file"
+            else
+                echo "ffmpeg command failed for file: $file"
+            fi
+            echo "done"
+        fi
+    done
+}
+
+# 处理右侧和左侧的文件
+process_files "$root/toRight" 1
+process_files "$root/toLeft" 2
