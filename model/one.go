3fdb46cb9040a78122b6153876076313903c3e90
zen <hasee>
2024-12-24 18:35:51 +0800

    标记用来下载的设备

--- model/one.go
+++ model/one.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"fmt"
+	"os"
+	"runtime"
 	"time"
 
 	"github.com/zhangyiming748/FastTdl/mysql"
@@ -16,6 +19,7 @@ type File struct {
 	Filename  string    `xorm:"comment('手动设置的文件名 @后面的文件名') VARCHAR(255)"`
 	Offset    int       `xorm:"comment('偏移量 如下载当前媒体之后第n个文件 +后面的数字') Int"`
 	Capacity  int       `xorm:"comment('下载当前文件和之后的n个文件 %后面的数字') Int"`
+	From      string    `xorm:"comment('用来下载的设备') VARCHAR(255)"`
 	CreatedAt time.Time `xorm:"created"`
 	UpdatedAt time.Time `xorm:"updated"`
 	DeletedAt time.Time `xorm:"deleted"`
@@ -31,6 +35,12 @@ func init() {
 }
 
 func (f *File) InsertOne() (int64, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown"
+	}
+	device := fmt.Sprintf("%v with %v:%v", hostname, runtime.GOOS, runtime.GOARCH)
+	f.From = device
 	return mysql.GetMysql().InsertOne(f)
 }
 
