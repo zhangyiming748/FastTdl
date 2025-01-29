7223ba73d7fcec97c4c31797ceb6205adb2a51c2
zen <mbp>
2025-01-29 19:45:39 +0800

    拆分参数

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -1,8 +1,10 @@
 package mysql
 
 import (
-	_ "github.com/go-sql-driver/mysql"
+	"fmt"
 	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 	"xorm.io/xorm"
 )
 
@@ -14,7 +16,13 @@ var (
 // CREATE DATABASE `tdl` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
 func SetMysql() {
 	var err error
-	engine, err = xorm.NewEngine("mysql", "root:163453@tcp(192.168.1.9:3306)/tdl?charset=utf8")
+	user := "root"
+	password := "163453"
+	host := "192.168.1.9"
+	port := 3306
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/tdl?charset=utf8", user, password, host, port)
+
+	engine, err = xorm.NewEngine("mysql", dataSourceName)
 	if err != nil {
 		useMysql = false
 	} else {
